ff60812490b4d157b64f269e8f23ac86e71bc912
Jack Younger <6219292+jackwade@users.noreply.github.com>
2020-07-12 15:49:43 -0700

    refactor

--- index.js
+++ index.js
@@ -1,46 +1,131 @@
-// Variables to store individual game stats
-let monroeChosenTeam
-let cincoChosenTeam
-let monroeFinalScore
-let cincoFinalScore
-let monroeTotalWins = 0
-let cincoTotalWins = 0
-let pad = '00';
 
+// App state
+const state = {
+    game: {},
+    users: [
+        {
+            id: 'monroeId',
+            name: 'Monroe',
+            totalWins: 0,
+        },
+        {
+            id: 'cincoId',
+            name: 'Cinco',
+            totalWins: 0,
+        }
+    ],
+    games: [
+        {
+            playerOneId: 'monroeId',
+            playerTwoId: 'cincoId',
+            playerOneTeam: 'ATL',
+            playerTwoTeam: 'HOU',
+            playerOneScore: 55,
+            playerTwoScore: 56,
+        },
+        {
+            playerOneId: 'monroeId',
+            playerTwoId: 'cincoId',
+            playerOneTeam: 'LAL',
+            playerTwoTeam: 'MIN',
+            playerOneScore: 65,
+            playerTwoScore: 64,
+        },
+        {
+            playerOneId: 'monroeId',
+            playerTwoId: 'cincoId',
+            playerOneTeam: 'NYK',
+            playerTwoTeam: 'BKN',
+            playerOneScore: 75,
+            playerTwoScore: 74,
+        },
+    ]
+};
+
+// TODO on document load, get games from database, put in state, remove mock data
+// API call that loads data
+// iterate through the games and calculate totalWins for each user
+// games.reduce((acc, cur) => {
+//     // sum up all the wins - maybe use getWinner function
+// }, { playerOneWins: 0, playerTwoWins: 0 })
+
+// Game factory creates game
+const createGame = () => ({
+    playerOneId: 'monroeId',
+    playerTwoId: 'cincoId',
+    playerOneTeam: $('#monroe-team').val(),
+    playerTwoTeam: $('#cinco-team').val(),
+    playerOneScore: $('#monroe-score').val(),
+    playerTwoScore: $('#cinco-score').val(),
+});
+
+// Show and hide game entry form
 function showForm() {
     document.getElementById('add-game-form').style.display = 'block';
 }
-
 function hideForm() {
     document.getElementById('add-game-form').style.display = 'none';
 }
 
-$('a#submit').on('click', function() {
-    monroeChosenTeam = document.getElementById("monroe-team").value;
-    monroeFinalScore = document.getElementById("monroe-score").value;
-    cincoChosenTeam = document.getElementById("cinco-team").value;
-    cincoFinalScore = document.getElementById("cinco-score").value;
+// Submit game entry form
+$('a#submit').on('click', () => {
+    const game = createGame();
 
     hideForm();
-    updateScore(monroeFinalScore, cincoFinalScore);
-    updateMarquee();
-})
+    updateWins(game);
+    displayWins(game);
+    updateMarquee(game);
+});
 
-function updateScore(monroeFinalScore, cincoFinalScore) {
-    if (Number(monroeFinalScore) > Number(cincoFinalScore)) {
-        monroeTotalWins++
-    } else {
-        cincoTotalWins++
-    }
-    let monroeTotalWinsPadded = (pad + String(monroeTotalWins)).slice(-pad.length);
-    let cincoTotalWinsPadded = (pad + String(cincoTotalWins)).slice(-pad.length);
-    document.getElementById("left-score").innerHTML = monroeTotalWinsPadded;
-    document.getElementById("right-score").innerHTML = cincoTotalWinsPadded;
+// Determines the winner of a game and returns winner ID
+function getWinnerId(game) {
+    const finalScoreOneInt = parseInt(game.playerOneScore, 10);
+    const finalScoreTwoInt = parseInt(game.playerTwoScore, 10);
+
+    return finalScoreOneInt > finalScoreTwoInt
+        ? game.playerOneId
+        : game.playerTwoId;
 }
 
-function updateMarquee() {
-   let marquee = document.querySelector("marquee")
-   marquee.innerHTML = monroeChosenTeam.toUpperCase() + " VS " +
-                       cincoChosenTeam.toUpperCase() + ": " +
-                       monroeFinalScore + "-" + cincoFinalScore;
+// Update the total wins prop on the user
+function updateWins(game) {
+    const winnerId = getWinnerId(game);
+    const winner = state.users.find(user => user.id === winnerId)
+
+    winner.totalWins++
+}
+
+// Shows the wins on the scoreboard
+function displayWins(game) {
+    const { users } = state;
+    const { playerOneId, playerTwoId } = game;
+
+    const playerOne = users.find(user => user.id === playerOneId)
+    const playerTwo = users.find(user => user.id === playerTwoId)
+
+    // TODO make player name dynamic
+    // $('#player-one-name').text(playerOne.name.toUpperCase());
+
+    // TODO refactor
+    const playerOneTotalWinsStr = ('00' + playerOne.totalWins.toString()).slice(-2);
+    const playerTwoTotaWinsStr = ('00' + playerTwo.totalWins.toString()).slice(-2);
+    
+    $("#left-score").text(playerOneTotalWinsStr);
+    $("#right-score").text(playerTwoTotaWinsStr);
+}
+
+// Updates the scrolling marquee under the scoreboard
+function updateMarquee(game) {
+    const { playerOneScore, playerTwoScore, playerOneTeam, playerTwoTeam } = game;
+    const marquee = document.querySelector("marquee");
+    const text = `
+        ${playerOneTeam.toUpperCase()} VS ${playerTwoTeam.toUpperCase()}
+        : ${playerOneScore} - ${playerTwoScore}
+    `;
+
+    // marquee.innerHTML = playerOneTeam.toUpperCase() + " VS " +
+    //                     playerTwoTeam.toUpperCase() + ": " +
+    //                     playerOneScore + "-" + playerTwoScore;
+
+    marquee.text(text);
 }

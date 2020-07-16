5378d6281166b553431c51e5ca5b3d511407d95d
Cinco45 <59041306+Cinco45@users.noreply.github.com>
2020-07-16 13:17:06 -0700

    reupload

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
-// ************************* //
-// CONFIGURATION AND GLOBALS //
-// ************************* //
-
-(function () {
-    const firebaseConfig = {
-
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-})()
-
-// Database & State variables
-const auth = firebase.auth();
+// Database & State
 const db = firebase.database();
 const dbGames = db.ref('/games/');
 const dbUsers = db.ref('/users/');
 const state = [];
 
-// ********************************* //
-// FUNCTIONS FOR DATABASE READ/WRITE //
-// ********************************* //
-
-// Retrieves data of previous games from database
-dbGames.on('value', function (snapshot) {
+// API call that loads data
+dbGames.on('value', function(snapshot) {
     state.length = 0;
-    snapshot.forEach(function (childSnapshot) {
+    snapshot.forEach(function(childSnapshot) {
         state.push(childSnapshot.val());
         calcTotalWins();
         calcTotalPoints();
         calcBlowouts();
         updateMarquee();
     });
+});    
+
+$("#add-game-btn").hover(function() {
+    $(this).addClass("hvr-bob");
+    }, function() {
+        $(this).removeClass("hvr-bob");
 });
 
-// Submit button calls a write to the database
-$('a#btn-submit').on('click', () => {
+// Show and hide game entry form
+function showForm() {
+    $('#add-game-form').addClass('hvr-bounce-in').show();
+}
+function hideForm() {
+    $('#add-game-form').hide();
+}
+
+// Submit game entry form
+$('a#submit').on('click', () => {
     writeGame();
     hideForm();
 });
 
 // Writes a new game to the database
-function writeGame() {
+function writeGame()  {
     dbGames.push({
         playerOneId: 'monroeId',
         playerTwoId: 'cincoId',
@@ -48,71 +45,14 @@ function writeGame() {
         playerTwoTeam: $('#player-two-team').val(),
         playerOneScore: $('#player-one-score').val(),
         playerTwoScore: $('#player-two-score').val(),
-    })
-}
-
-// **************************** //
-// FUNCTIONS FOR AUTHENTICATION //
-// **************************** //
-
-// Add login event
-function login() {
-    const email = document.getElementById('txt-email').value;
-    const pass = document.getElementById('txt-password').value;
-
-    // Sign in
-    auth.signInUserWithEmailAndPassword(email,
-        pass).then(function (user) {
-        var user = firebase.auth().currentUser;
-        console.log(user); // Optional
-    }, function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
-}
-
-// Add signup event
-function signUp() {
-    const email = document.getElementById('txt-email').value;
-    const pass = document.getElementById('txt-password').value;
-
-    // Sign up
-    auth.createUserWithEmailAndPassword(email,
-        pass).then(function (user) {
-        var user = firebase.auth().currentUser;
-        console.log(user); // Optional
-    }, function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
-}
-
-// Add a realtime listener
-auth.onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(firebaseUser);
-        showAddGameForm();
-    } else {
-        console.log('not logged in');
-    }
-});
-
-// ***************************** //
-// FUNCTIONS THAT CALCULATE DATA //
-// ***************************** //
+})}
 
 // Gets all scores from state
 function getScores() {
-    const p1Scores = state.map(function ({
-        playerOneScore
-    }) {
+    const p1Scores = state.map(function({playerOneScore}) {
         return parseInt(playerOneScore, 10);
     });
-    const p2Scores = state.map(function ({
-        playerTwoScore
-    }) {
+    const p2Scores = state.map(function({playerTwoScore}) {
         return parseInt(playerTwoScore, 10);
     });
 
@@ -142,15 +82,15 @@ function calcTotalPoints() {
     const p1Scores = scores[0];
     const p2Scores = scores[1];
 
-    const p1TotalPoints = p1Scores.reduce(function (a, b) {
+    const p1TotalPoints = p1Scores.reduce(function(a, b) {
         return a + b;
     }, 0);
 
-    const p2TotalPoints = p2Scores.reduce(function (a, b) {
+    const p2TotalPoints = p2Scores.reduce(function(a, b) {
         return a + b;
     }, 0);
 
-    displayTotalPoints(p1TotalPoints, p2TotalPoints);
+     displayTotalPoints(p1TotalPoints, p2TotalPoints);
 }
 
 // Calculates number of blowouts for each user
@@ -176,16 +116,11 @@ function calcBlowouts() {
     displayBlowouts(p1Blowouts, p2Blowouts);
 }
 
-// *********************************** //
-// FUNCTIONS THAT DISPLAY DATA ON SITE //
-// *********************************** //
-
-
 // Displays total wins for each player on scoreboard
 function displayWins(p1TotalWins, p2TotalWins) {
     const p1TotalWinsStr = ('00' + p1TotalWins.toString()).slice(-2);
     const p2TotalWinsStr = ('00' + p2TotalWins.toString()).slice(-2);
-
+    
     $("#home-wins").text(p1TotalWinsStr);
     $("#guest-wins").text(p2TotalWinsStr);
 }
@@ -196,7 +131,6 @@ function displayTotalPoints(p1TotalPoints, p2TotalPoints) {
     $("#p2-tot-pts").text(p2TotalPoints);
 }
 
-// Displays total number of blowouts for each player on scoreboard
 function displayBlowouts(p1Blowouts, p2Blowouts) {
     $("#p1-blowouts").text(p1Blowouts);
     $("#p2-blowouts").text(p2Blowouts);
@@ -204,67 +138,16 @@ function displayBlowouts(p1Blowouts, p2Blowouts) {
 
 // Displays teams & scores of latest game on scrolling marquee
 function updateMarquee() {
-    dbGames.limitToLast(1).on('value', function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
+    dbGames.limitToLast(1).on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
             const game = childSnapshot.val();
 
             $('marquee').text(`
             ${game.playerOneTeam.toUpperCase()} VS ${game.playerTwoTeam.toUpperCase()}
             : ${game.playerOneScore} - ${game.playerTwoScore}
             `);
-        });
-    })
-}
-
-
-
-// Function Below is to show and hide the stats button on names
-function show_hide() {
-    var click = document.getElementById("drop-content");
-    if (click.style.display === "none") {
-        click.style.display = "block";
-    } else {
-        click.style.display = "none";
-    }
-}
-
-
-// ************************************ //
-// FUNCTIONS THAT ADD ANIMATION TO SITE //
-// ************************************ //
-
-// Show and hide game entry form
-function showAddGameForm() {
-    $('#add-game-form').addClass('hvr-bounce-in').show();
-}
-
-function hideForm() {
-    $('#add-game-form').hide();
-}
-
-function showLoginForm() {
-    $('#auth-form-container').show();
-}
-
-function hideLoginForm() {
-    $('#auth-form-container').hide();
-}
-
-// Hover animation for the Add Game button
-$("#add-game-btn").hover(function () {
-    $(this).addClass("hvr-bob");
-}, function () {
-    $(this).removeClass("hvr-bob");
-});
-
-
-
-
-
-
-
-
-
+    });
+})}
 
 // Determines the winner of a game and returns winner ID
 // function getWinnerId(game) {
